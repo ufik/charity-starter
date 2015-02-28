4b2fcaf84a67da1eccef36006f05482ca7db73b5
Tomas Voslar <t.voslar@gmail.com>
2015-02-28 11:51:34 +0000

    Added jshint file, refactoring.

--- api/controllers/CompanyController.js
+++ api/controllers/CompanyController.js
@@ -4,38 +4,34 @@
  * @description :: Server-side logic for managing Companies
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
+
+'use strict';
+
 var passport = require('passport');
 
 module.exports = {
-    passport_local: function(req, res)
-    {
-        passport.authenticate('local', function(err, company, info)
-        {
-            if ((err) || (!company))
-            {
-                res.redirect('/login');
-                return;
-            }
+  passportLocal: function (req, res) {
+    passport.authenticate('local', function (err, company, info) {
+      if (err || !company) {
+        res.redirect('/login');
+        return;
+      }
 
-            req.logIn(company, function(err)
-            {
-                if (err)
-                {
-                    res.redirect('/login');
-                    return;
-                }
+      req.logIn(company, function (err) {
+        if (err) {
+          res.redirect('/login');
+          return;
+        }
 
-                res.redirect('/');
-                return;
-            });
-        })(req, res);
-    },
+        res.redirect('/');
+        return;
+      });
+    })(req, res);
+  },
 
   /**
    * Overrides for the settings in `config/controllers.js`
    * (specific to CompanyController)
    */
   _config: {}
-
-
 };
--- api/models/Company.js
+++ api/models/Company.js
@@ -1,48 +1,49 @@
 /**
-* Company.js
-*
-* @description :: TODO: You might write a short summary of how this model works and what it represents here.
-* @docs        :: http://sailsjs.org/#!documentation/models
-*/
+ * Company.js
+ *
+ * @description :: Company model.
+ * @docs        :: http://sailsjs.org/#!documentation/models
+ */
 
-var bcrypt = require('bcrypt')
+'use strict';
+
+var bcrypt = require('bcrypt');
 var SALT_WORK_FACTOR = 10;
-var MIN_PASSWORD_LENGTH = 8;
 
 module.exports = {
-  	attributes: {
-		name: {
-			type: 'string',
-			required: true	
-		},
-  		address: {
-  			type: 'text',
-  			required: true
-  		},
-  		phone:{
-		   type: 'string',
-		   defaultsTo: '000-000-0000'
-		},
-		password: {
-  			type: 'string',
-  			required: true,
-  			minLength: 8
-  		},
-  		email: {
-		   type: 'email',
-		   unique: true,
-		   required: true
-		}
-	},
+  attributes: {
+    name: {
+      type: 'string',
+      required: true
+    },
+    address: {
+      type: 'text',
+      required: true
+    },
+    phone: {
+      type: 'string',
+      defaultsTo: '000-000-0000'
+    },
+    password: {
+      type: 'string',
+      required: true,
+      minLength: 8
+    },
+    email: {
+      type: 'email',
+      unique: true,
+      required: true
+    }
+  },
 
-	beforeCreate: function(values, cb) {
-		bcrypt.hash(values.password, SALT_WORK_FACTOR, function(err, hash){
-            if(err) {
-            	throw err;
-            }
+  beforeCreate: function (values, cb) {
+    bcrypt.hash(values.password, SALT_WORK_FACTOR, function (err, hash) {
+      if (err) {
+        throw err;
+      }
 
-            values.password = hash;
-            cb();
-        });
-	  }
+      values.password = hash;
+      cb();
+    });
+  }
 };
