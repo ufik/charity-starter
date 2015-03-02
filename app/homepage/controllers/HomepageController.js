95910b00ec2ce21a74508b67cd0c260252912fe9
Tomas Voslar <t.voslar@gmail.com>
2015-03-02 21:38:28 +0000

    Added user list.

--- app/homepage/controllers/HomepageController.js
+++ app/homepage/controllers/HomepageController.js
@@ -1,5 +1,6 @@
 if (Meteor.isClient) {
 	Meteor.subscribe("project");
+	Meteor.subscribe("userStatus");
 
 	HomeController = RouteController.extend({
 		layoutTemplate: 'AppLayout',
@@ -11,5 +12,29 @@ if (Meteor.isClient) {
 	      	}
 	      });
 	    }
+	});
+
+	Template.user.helpers({
+		labelClass: function() {
+			console.log(this);
+		  if (this.status && this.status.idle)
+		    return "label-warning"
+		  else if (this.status && this.status.online)
+		    return "label-success"
+		  else
+		    return "label-default"
+		}
+	});
+
+	Template.AppLayout.helpers({
+		usersOnline: function() {
+		  return Meteor.users.find();
+		}
+	});
+}
+
+if (Meteor.isServer) {
+	Meteor.publish("userStatus", function() {
+	  return Meteor.users.find();
 	});
 }
